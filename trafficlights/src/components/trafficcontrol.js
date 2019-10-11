ddef461b1320acb0c1cb05b8a1a800d6392f489e
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-10-11 21:08:02 +0000

    trafficlights commit4

--- trafficlights/src/components/trafficcontrol.js
+++ trafficlights/src/components/trafficcontrol.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './lights.css';
 
@@ -6,38 +6,45 @@ const ControlTraff = () => {
 
    // const [time, setTime] = useState(0);
     const [flag, setFlag] = useState(0);
+    const [time, setTime] = useState(0);
 
-                const onRed = () => {
-                    document.getElementById("redone").style.opacity = "1";
-                    document.getElementById("yellowone").style.opacity = ".3";
-                    document.getElementById("greenone").style.opacity = ".3";
-                    document.getElementById("walk").style.opacity = "1";
-                    document.getElementById("stop").style.opacity = ".3";
-                    }
-                const onYellow = () => {
-                    document.getElementById("redone").style.opacity = ".3";
-                    document.getElementById("yellowone").style.opacity = "1";
-                    document.getElementById("greenone").style.opacity = ".3";
-                    document.getElementById("walk").style.opacity = ".3";
-                    document.getElementById("stop").style.opacity = "1";
-                    }
-                const onGreen = () => {
-                    document.getElementById("redone").style.opacity = ".3";
-                    document.getElementById("yellowone").style.opacity = ".3";
-                    document.getElementById("greenone").style.opacity = "1";
-                    document.getElementById("walk").style.opacity = ".3";
-                    document.getElementById("stop").style.opacity = "1";
-                    }
+    const onRed = () => {
+        document.getElementById("redone").style.opacity = "1";
+        document.getElementById("yellowone").style.opacity = ".3";
+        document.getElementById("greenone").style.opacity = ".3";
+        document.getElementById("walk").style.opacity = "1";
+        document.getElementById("stop").style.opacity = ".3";
+    }
+    const onYellow = () => {
+        document.getElementById("redone").style.opacity = ".3";
+        document.getElementById("yellowone").style.opacity = "1";
+        document.getElementById("greenone").style.opacity = ".3";
+        document.getElementById("walk").style.opacity = ".3";
+        document.getElementById("stop").style.opacity = "1";
+    }
+    const onGreen = () => {
+        document.getElementById("redone").style.opacity = ".3";
+        document.getElementById("yellowone").style.opacity = ".3";
+        document.getElementById("greenone").style.opacity = "1";
+        document.getElementById("walk").style.opacity = ".3";
+        document.getElementById("stop").style.opacity = "1";
+    }
 
+    useEffect(() => {
 
-                const onAuto = () => {
-                    setFlag(1);
-                 if (flag === 1) {
-                    onRed();
-                    onYellow();
-                    onGreen();
-                }
+        setTimeout(() => setTime(time + 1), 1000);
+
+        if (flag === 1) {
+            if (time === 2)  onRed();
+            if (time === 4)  onYellow();
+            if (time === 6)  onGreen();
+            if (time === 7)  setTime(0);
         }
+
+    }, [time])
+
+
+
 
 	return (
        <>
@@ -54,11 +61,11 @@ const ControlTraff = () => {
         <div className="buttonlights">
 			<div className="lightson" ><button>Lights On </button></div>
             <div className="lightsoff"><button>Lights Off </button></div>
-            <div className="automatic"onClick={onAuto}><button>Automatic </button></div>
+            <div className="automatic" onClick={setFlag(1)}><button>Automatic </button></div>
 		</div>
       </>
 	);
-};
 
+};
 
 export default ControlTraff;
